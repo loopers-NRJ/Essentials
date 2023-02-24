9a218ac4aeba76f963f69fe9edc5581cd141a5b6
jayakarthik-jk <karthick72002@gmail.com>
2023-02-24 07:51:23 +0530

    added ss props

--- pages/products/[productId].tsx
+++ pages/products/[productId].tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { NextApiRequest, NextApiResponse } from "next";
+import { Products } from "@prisma/client";
+import { getProductById } from "../../database/products";
 
-function ProductDetails() {
+function ProductDetails({ product }: { product: Products }) {
+  console.log(product);
   return <div>productId</div>;
+}
+
+export async function getServerSideProps(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const product = await getProductById(req.query.productId as string);
+  console.log(product);
+  if (!product || product instanceof Error) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: {
+      product: JSON.parse(JSON.stringify(product)),
+    },
+  };
 }
 
 export default ProductDetails;
--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,8 +1,36 @@
 import React from "react";
-import Footer from "../../Components/Footer";
+import { NextApiRequest, NextApiResponse } from "next";
+import { Products } from "@prisma/client";
+import { getProducts } from "../../database/products";
 
-function index() {
-  return <div>index</div>;
+function index({ products }: { products: Products[] }) {
+  console.log(products);
+  return (
+    <div>
+      <ol>
+        {products.map((p) => (
+          <li key={p.id}>{p.name}</li>
+        ))}
+      </ol>
+    </div>
+  );
+}
+
+export async function getServerSideProps(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const query = (req.query.query as string) || "";
+  const limit = (req.query?.limit && +req.query.limit) || 30;
+  const page = (req.query?.page && +req.query.page) || 1;
+  const filter = (req.query?.filter as string) || "";
+  const sort = (req.query.sort as string) || "name_asc";
+  const products = await getProducts({ query, limit, page, sort, filter });
+  return {
+    props: {
+      products: JSON.parse(JSON.stringify(products)),
+    },
+  };
 }
 
 export default index;
