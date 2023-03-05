3d5353dcfb827ce72bb04902b10126c38bd4e96e
Mohamed Rizwan <mynameisrizwan35@gmail.com>
2023-03-05 19:57:46 +0530

    z-index bug fix

--- src/Components/Common/Layout.tsx
+++ src/Components/Common/Layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-[100vh] px-8 md:px-10 lg:px-20 w-full">
+    <div className="min-h-[100vh] px-8 md:px-10 lg:px-20 w-full z-20">
       <Navbar />
       {children}
     </div>
--- src/Components/Common/Navbar.tsx
+++ src/Components/Common/Navbar.tsx
@@ -2,8 +2,8 @@ import Navlink from "./NavLink";
 
 function Navbar() {
   return (
-    <div className="flex fixed top-0 left-0 right-0 p-5 md:px-10 lg:px-28 w-full">
-      <nav className="flex items-center gap-4 px-10 py-4 bg-gradient-to-br from-white/20 to-white/10 backdrop-blur-[10px] shadow-lg rounded-lg text-sm mx-auto w-full">
+    <div className="flex fixed top-0 left-0 right-0 p-5 md:px-10 lg:px-28 w-full z-20">
+      <nav className="flex items-center gap-4 px-10 py-4 bg-gradient-to-br from-white/20 to-white/10 backdrop-blur-[10px] shadow-lg rounded-lg text-sm mx-auto w-full ">
         <Navlink link="">Home</Navlink>
         <Navlink link="">Products</Navlink>
         <Navlink link="">About Us</Navlink>
